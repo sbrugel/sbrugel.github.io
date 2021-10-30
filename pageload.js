96d9d7bc53fb2c9d180ee6987d6792c77423469e
sbrugel <sbrugel62@gmail.com>
2021-10-30 14:55:52 -0400

    feat: blinking text; 2010s scenarios added

--- pageload.js
+++ pageload.js
@@ -1,4 +1,9 @@
 $().ready(function() {
 	$("#nav").load("https://raw.githubusercontent.com/sbrugel/sbrugel.github.io/main/nav.html"); 
 	$("#footer").load("https://raw.githubusercontent.com/sbrugel/sbrugel.github.io/main/footer.html"); 
+	var blink_speed = 750;
+	var t = setInterval(function () {
+		var ele = document.getElementById('blinkingDiv');
+		ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+	}, blink_speed);
 });
